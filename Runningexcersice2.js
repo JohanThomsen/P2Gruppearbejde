97169b22b7ed7f9de0d053a7589c2bee08139c9c
frederikhm <1637511+frederikhm@users.noreply.github.com>
2020-02-11 15:13:41 +0100

    lavet til funktion

--- Runningexcersice2.js
+++ Runningexcersice2.js
@@ -1,14 +1,16 @@
 let Expense = ["Tyrion", "Wine", 69, "Eur", "20/04"];
 
+
+function addRow(id, array){
 //we have access to the HTML document, and it is easy accesible by tableElem.
 let tr = document.createElement("tr");
-let th = document.createElement("th");
-let elem = document.getElementById("listExpense");
+let elem = document.getElementById(id);
 elem.appendChild(tr);
-elem.lastChild.innerHTML = `<th> ${Expense[0]} </th>
-                            <th> ${Expense[1]} </th>
-                            <th> ${Expense[2]} </th>
-                            <th> ${Expense[3]} </th>
-                            <th> ${Expense[4]} </th>`;
+elem.lastChild.innerHTML = `<th> ${array[0]} </th>
+                            <th> ${array[1]} </th>
+                            <th> ${array[2]} </th>
+                            <th> ${array[3]} </th>
+                            <th> ${array[4]} </th>`;
+}
 
-
+addRow("listExpense", Expense);
